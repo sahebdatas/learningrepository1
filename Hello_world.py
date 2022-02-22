d02c4673d3238e8adc2f7908ffc59021dac630ef
Saheb <saheb.mukherjee.off@gmail.com>
2022-02-22 07:46:04 +0530

    Saheb branch updated with odd even prog

--- Hello_world.py
+++ Hello_world.py
@@ -1,3 +1,9 @@
 # Creator name: Saheb Mukherjee
 # Date: 22 feb 2022
 print("Hello World...")
+
+a=int(input("Enter a num to verify odd/even:"))
+if(a%2==0):
+    print("The num is even..")
+else:
+    print("The number is odd..")
